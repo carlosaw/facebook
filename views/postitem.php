af34c803da0207e6afd8cba0f8fe56e2c2b00011
unknown <carlosfreevg@gmail.com>
2021-10-15 22:01:21 -0400

    Visual

--- views/postitem.php
+++ views/postitem.php
@@ -1,4 +1,7 @@
 <div class="postitem">
+  <div class="postitem_info">
+    <strong>Post de:</strong> <?php echo $nome; ?>
+  </div>
   <?php if($tipo == 'foto'): ?>
   <img src="<?php echo BASE; ?>assets/images/posts/<?php echo $url; ?>" border="0" width="100%" />
   <?php endif; ?>
@@ -6,14 +9,12 @@
   <div class="postitem_texto">
     <?php echo $texto; ?>
   </div>
-  <div class="postitem_info">
-    <strong>Post de:</strong> <?php echo $nome; ?>
-  </div>
+  
   <div class="postitem_botoes">
     <button class="btn btn-default" onclick="curtir(this)" data-id="<?php echo $id; ?>" data-likes="<?php echo $likes; ?>" data-liked="<?php echo $liked; ?>">(<?php echo $likes; ?>) <?php echo ($liked=='0')?'Curtir':'Descurtir'; ?></button>
 
     <button class="btn btn-default" onclick="displayComentario(this)">Comentar</button><br/><br/>
-    <div class="postitem_comentario">
+    <div class="postitem_comentario"><br/>
       <input type="text" class="postitem_txt form-control" /><br/>
       <button class="btn btn-default" data-id="<?php echo $id; ?>" onclick="comentar(this)">Enviar</button><br/>
     </div>
